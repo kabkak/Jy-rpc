725794067de2d2df0b4f111f5273bfe7e96e576b
jiangYing <3307494769@qq.com>
2024-07-23 18:07:34 +0800

    完成web服务器 监听端口

---
+++ Jy-rpc-easy/src/main/java/com/jiangying/Jyrpc/server/VertxHttpServer.java
@@ -0,0 +1,26 @@
+package com.jiangying.Jyrpc.server;
+
+import io.vertx.core.Vertx;
+
+public class VertxHttpServer implements HttpServer {
+    @Override
+    public void doStart(int port) {
+        Vertx vertx = Vertx.vertx();
+        io.vertx.core.http.HttpServer httpServer = vertx.createHttpServer();
+        httpServer.requestHandler(request -> {
+
+            System.out.println("收到请求" + request.method() + request.uri());
+            request.response()
+                    .putHeader("content-type", "text/html;charset=utf-8")
+                    .end("<h1>你好你好你好你好你好</h1>");
+        });
+        httpServer.listen(port, result -> {
+            if (result.succeeded()) {
+                System.out.println("Server listening at " + port);
+            } else {
+                System.out.println("Failed to bind!");
+            }
+        });
+
+    }
+}
--- provider/src/main/java/com/jiangying/provider/EasyProvider.java
+++ provider/src/main/java/com/jiangying/provider/EasyProvider.java
@@ -1,5 +1,9 @@
 package com.jiangying.provider;
 
+import com.jiangying.Jyrpc.server.HttpServer;
+import com.jiangying.Jyrpc.server.VertxHttpServer;
+
+
 public class EasyProvider {
     /**
      * 简答提供服务
@@ -7,6 +11,8 @@ public class EasyProvider {
      */
     public static void main(String[] args) {
         //todo 提供服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
 
     }
 }
